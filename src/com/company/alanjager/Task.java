4c4f502b938dceae9511ec99f06f485904506bbd
AlanJager <873863981@qq.com>
2017-01-24 14:52:53 +0800

    simple thread pool design

---
+++ src/com/company/alanjager/Task.java
@@ -0,0 +1,17 @@
+package com.company.alanjager;
+
+/**
+ * Created by AlanJager on 2017/1/24.
+ */
+public class Task {
+    int argument;
+
+
+    public int getArgument() {
+        return argument;
+    }
+
+    public void setArgument(int argument) {
+        this.argument = argument;
+    }
+}
---
+++ src/com/company/alanjager/WorkThread.java
@@ -0,0 +1,28 @@
+package com.company.alanjager;
+
+/**
+ * Created by AlanJager on 2017/1/24.
+ */
+public class WorkThread extends Thread {
+
+    Task task;
+
+    private void process() {
+        System.out.println("process argument: " + task.getArgument());
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+" Start. argument = " + this.task.getArgument());
+        processCommand();
+        System.out.println(Thread.currentThread().getName()+" End.");
+    }
+
+    private void processCommand() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
