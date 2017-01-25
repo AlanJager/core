3bc3aa8a94f60336040f4b5d858639ffaa5a6498
AlanJager <873863981@qq.com>
2017-01-25 15:48:23 +0800

    get message channel from pool

--- src/com/company/messageBus/MessageBusImpl.java
+++ src/com/company/messageBus/MessageBusImpl.java
@@ -9,23 +9,35 @@ import java.io.IOException;
  */
 public class MessageBusImpl implements MessageBus{
     private final static String QUEUE_NAME = "message_bug";
+    private ChannelPool channelPool;
+    MessageBusImpl() {
+        init();
+    }
 
-    @Override
-    public void send(Message message) {
+    private void init() {
         try {
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost("localhost");
             Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
+            channelPool = new ChannelPool(connection);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public void send(Message message) {
+        try {
+            Channel channel = channelPool.get();
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             channel.basicPublish("", QUEUE_NAME, null, message.getMessage().getBytes());
             System.out.println(" [x] Sent '" + message + "'");
 
-            channel.close();
-            connection.close();
+            channelPool.returnChannel(channel);
         } catch (IOException e) {
             System.out.println("Send message error through queue: " + QUEUE_NAME);
+        } catch (InterruptedException e) {
+            System.out.println("Get channel from pool interrupt");
         }
     }
 
@@ -37,11 +49,7 @@ public class MessageBusImpl implements MessageBus{
     @Override
     public String receive() {
         try {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            Connection connection = factory.newConnection();
-            Channel channel = null;
-            channel = connection.createChannel();
+            Channel channel = channelPool.get();
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
             Consumer consumer = new DefaultConsumer(channel) {
@@ -55,6 +63,8 @@ public class MessageBusImpl implements MessageBus{
             channel.basicConsume(QUEUE_NAME, true, consumer);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.out.println("Get channel from pool interrupt");
         }
 
         return null;
